242e749e887d05ea6d5a7a0b3484c7405afbc9a1
josuemal98 <jmaldonadofuentes@byupathway.edu>
2026-07-31 21:37:19 -0600

    week06

---
+++ final/scripts/site-plan.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const currentYearSpan = document.getElementById("currentYear");
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+
+    const lastModifiedParagraph = document.getElementById("lastModified");
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+    }
+});
